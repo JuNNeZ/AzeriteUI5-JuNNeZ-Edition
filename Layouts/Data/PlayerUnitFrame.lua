7483fd8ba5bd08438c862abc623f62cb16bd6f10
JuNNeZ <you@example.com>
2026-03-04 20:41:51 +0100

    Revert "fix(player): decouple power bar from backdrop sizing and move backdrop Y -9"
    This reverts commit d6335b538695f8a528b7ada298bd6de390b4cefe.

--- Layouts/Data/PlayerUnitFrame.lua
+++ Layouts/Data/PlayerUnitFrame.lua
@@ -228,7 +228,7 @@ ns.RegisterConfig("PlayerFrame", {
 		PowerBarSparkMap = crystalSparkMap,
 
 		PowerBackdropSize = { 196, 196 },
-		PowerBackdropPosition = { "CENTER", 0, -9 },
+		PowerBackdropPosition = { "CENTER", 0, 0 },
 		PowerBackdropTexture = GetMedia("power_crystal_back"),
 		PowerBackdropTextureWrath = GetMedia("power-crystal-ice-back"),
 
